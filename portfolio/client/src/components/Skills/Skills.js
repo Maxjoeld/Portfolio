1922791aa42bfbdc8ea58d84a5332c0608558ae1
Maximo DeLaRosa <mjd809@gmail.com>
2018-08-31 13:56:19 -0400

    Finished skills page
    Still needs slight adjustments but overall layout is complete

--- portfolio/client/src/components/Skills/Skills.js
+++ portfolio/client/src/components/Skills/Skills.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import Navigation from '../Navigation/Navigation';
 import './Skills.css'
+import C from './C.png';
+import javascript from './javascript.png';
+import mongo from './mongo.png';
+import sass from './sass.png';
+import sequelize from './sequelize.png';
+import python from './python.png';
+import graph from './graphql.png';
+import nodejs from './nodejs.png';
+import django from './djangogr.png';
+import redux from './redux.png';
+import express from './Expressjs.png';
 
 class Skills extends Component {
   state = {  }
@@ -19,9 +30,17 @@ class Skills extends Component {
               </p>
             </div>
             <div className="Project">
-              <div className="Project-box">Project one</div>
-              <div className="Project-box">Project two</div>
-              <div className="Project-box">Project three</div>
+              <img style={{maxWidth: '4rem', margin: '0.5rem'}}src={C} alt="C" />
+              <img style={{maxWidth: '4rem', margin: '0.5rem'}}src={javascript} alt="C" />
+              <img style={{maxWidth: '4rem', margin: '0.5rem'}}src={mongo} alt="C" />
+              <img style={{maxWidth: '4rem', margin: '0.5rem'}}src={sass} alt="C" />
+              <img style={{maxWidth: '4rem', margin: '0.5rem'}}src={sequelize} alt="C" />
+              <img style={{maxWidth: '4rem', margin: '0.5rem'}}src={python} alt="C" />
+              <img style={{maxWidth: '4rem', margin: '0.5rem'}}src={graph} alt="C" />
+              <img style={{maxWidth: '4rem', margin: '0.5rem'}}src={nodejs} alt="C" />
+              <img style={{maxWidth: '4rem', margin: '0.5rem'}}src={django} alt="C" />
+              <img style={{maxWidth: '4rem', margin: '0.5rem'}}src={redux} alt="C" />
+              <img style={{maxWidth: '4rem', margin: '0.5rem'}}src={express} alt="C" />
             </div>
           </div>
         </div>
