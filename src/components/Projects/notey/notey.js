925868f90d16a774d0cc40b64678a51596b6f36e
Maximo DeLaRosa <mjd809@gmail.com>
2018-09-13 12:31:04 -0400

    Slideshow is working
    Still need to find a way to hide other slides

--- src/components/Projects/notey/notey.js
+++ src/components/Projects/notey/notey.js
@@ -8,20 +8,48 @@ import '../Projects.css';
 
 class Giveme extends Component {
   state = { 
-    slideIndex: 0,
-    loading: true,
-    autoplay: true,
+    images: [ signin, todo ],
+    translateValue: 0,
+    currentIndex: 0,
    }
 
-   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ loading: false, autoplay: false });
-    }, 50);
+   goToPrevSlide = () => {
+    // if(this.state.currentIndex === this.state.images.length - 1) {
+    //   return this.setState({
+    //     currentIndex: 0,
+    //     translateValue: 0
+    //   })
+    // }
+
+    // This will not run if we met the if condition above
+    this.setState(prevState => ({
+      currentIndex: prevState.currentIndex - 1,
+      translateValue: prevState.translateValue + (this.slideWidth())
+    }));
    }
 
-   componentWillMount() {
-     this.setState({ autoplay: true})
-   }
+   goToNextSlide = () => {
+    // Exiting the method early if we are at the end of the images array.
+    // We also want to reset currentIndex and translateValue, so we return
+    // to the first image in the array.
+    console.log('hey');
+    if(this.state.currentIndex === this.state.images.length - 1) {
+      return this.setState({
+        currentIndex: 0,
+        translateValue: 0
+      })
+    }
+    
+    // This will not run if we met the if condition above
+    this.setState(prevState => ({
+      currentIndex: prevState.currentIndex + 1,
+      translateValue: prevState.translateValue + -(this.slideWidth())
+    }));
+  }
+
+  slideWidth = () => {
+    return document.querySelector('.Slideshow-img').clientWidth;
+  }
 
 
   render() {
@@ -33,22 +61,30 @@ class Giveme extends Component {
       // visibility = 'visible';
       autoplay = false;
     }
+    console.log('hey');
     return (
       <React.Fragment>
-          <div className="Slideshow">
-              {/* {!loading ? */}
-                <img src={signin} className="Slideshow-img" alt="img" />
-                <img src={todo} className="Slideshow-img" alt="img" />
-              {/* :
-              <Carousel autoplay={autoplay} wrapAround={true} pauseOnHover={true} autoplayInterval={playInterval}>
-                <img src={signin} className="Slideshow-img" alt="img" />
-              </Carousel> 
-              } */}
+          <div className="Slideshow"style={{
+            transform: `translateX(${this.state.translateValue}px)`,
+            transition: 'transform ease-out 0.45s'
+          }}>>
+            {this.state.images.map((image, i) => (
+                <img key={i}src={image} className="Slideshow-img" alt="img" /> )
+              )   
+            }
           </div>
           {/* {loading ?
             <div class="lds-ring Loader"><div></div><div></div><div></div><div></div></div>
             : null } */}
             <div className="Slideshow-details">
+              <div className="Slideshow-arrows">
+                <div onClick={() => this.goToPrevSlide()}>
+                  <i className="fas fa-arrow-left fa-fw adjust" />
+                </div>
+                <div onClick={() => this.goToNextSlide()}>
+                  <i className="fas fa-arrow-left fa-fw opposite" />
+                </div>
+              </div>
               <div className="SlideShow-title">
                 <p>Notey</p>
                 <p className="Slideshow-details-sub">Note taking application</p>
