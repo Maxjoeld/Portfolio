3d91b5a21dbc6756616f090056ccef36616355c4
Maximo DeLaRosa <mjd809@gmail.com>
2018-09-14 16:05:32 -0400

    Added map to contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Map from './map';
 import './Contact.css';
 
 class Contact extends Component {
@@ -6,56 +7,59 @@ class Contact extends Component {
   render() { 
     return ( 
       <div className="Contact">
-        <p className="Contact-title">Contact Me</p>
-        <p className="Contact-subtext">If you have any questions, please feel free to contact me</p>
-        <form className="Contact-Form">
-        <div style={{display: 'flex'}}>
-          <input
-            className="Contact-Name"
-            placeholder="Name"
-            name="title"
-            type="text"
-            onChange={this.handleInputChange}
-            maxLength="32"
-            required
-          />
-          {/* <br /> */}
-          <input
-            className="Contact-Email"
-            placeholder="Email"
-            name="title"
-            type="text"
-            onChange={this.handleInputChange}
-            maxLength="32"
-            required
-          />
+        <div className="Contact-layout">
+          <p className="Contact-title">Contact Me</p>
+          <p className="Contact-subtext">If you have any questions, please feel free to contact me</p>
+          <form className="Contact-Form">
+            <div style={{display: 'flex'}}>
+              <input
+                className="Contact-Name"
+                placeholder="Name"
+                name="title"
+                type="text"
+                onChange={this.handleInputChange}
+                maxLength="32"
+                required
+              />
+              {/* <br /> */}
+              <input
+                className="Contact-Email"
+                placeholder="Email"
+                name="title"
+                type="text"
+                onChange={this.handleInputChange}
+                maxLength="32"
+                required
+              />
+            </div>
+            <input
+              className="Contact-Subject"
+              placeholder="Subject"
+              name="title"
+              type="text"
+              onChange={this.handleInputChange}
+              maxLength="32"
+              required
+            />
+            <textarea
+              className="Contact-message"
+              placeholder="Message"
+              name="content"
+              type="text"
+              onChange={this.handleInputChange}
+              required
+            />
+            <br />
+            <button
+              onClick={e => this.handleSubmit(e)}
+              className="Contact-submit"
+              type="submit"
+            >
+              Send
+            </button>
+          </form>
         </div>
-        <input
-          className="Contact-Subject"
-          placeholder="Subject"
-          name="title"
-          type="text"
-          onChange={this.handleInputChange}
-          maxLength="32"
-          required
-        />
-        <textarea
-          className="Contact-message"
-          placeholder="Message"
-          name="content"
-          type="text"
-          onChange={this.handleInputChange}
-          required
-        />
-        <br />
-        <button
-          onClick={e => this.handleSubmit(e)}
-          className="Contact-submit"
-          type="submit"
-        >
-          Send
-        </button>
-      </form>
+      <Map />
       </div>
      );
   }
---
+++ src/components/Contact/map.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+ 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+ 
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 40.70,
+      lng: -73.91
+    },
+    zoom: 11
+  };
+ 
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100%', width: '60%' }}>
+        <GoogleMapReact
+          // bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={40.703529}
+            lng={-73.911346}
+            text={'Kreyser Avrora'}
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+ 
+export default SimpleMap;
