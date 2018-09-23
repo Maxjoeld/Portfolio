fcf59f3427a5341a5e8cde756431978b21380dd0
Maximo DeLaRosa <mjd809@gmail.com>
2018-09-23 12:05:08 -0400

    Chnage sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,14 +7,14 @@ class Sidebar extends Component {
   render() { 
     return ( 
       <div className="Sidebar">
-        <div className="slideout">
+        {/* <div className="slideout">
           <span />
           <span />
           <span />
-        </div> 
+        </div>  */}
         <div className="bar">
           <a href="https://www.linkedin.com/in/maximo-delarosa/" target="_blank"  rel="noopener noreferrer">
-            <img className="one"src={linkedIn} alt="link" />
+            <img style={{color: 'black'}}className="one"src={linkedIn} alt="link" />
           </a>
           <a href="https://github.com/maxjoeld" target="_blank"  rel="noopener noreferrer"><i className="fab fa-github two"></i></a>
           <a href="https://medium.com/@mjd809" target="_blank"  rel="noopener noreferrer"><i className="fab fa-medium three"></i></a>
