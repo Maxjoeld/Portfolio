9e6be59fb4c4c7b0d1455c5cb7cca1aee996dd3d
Maximo DeLaRosa <mjd809@gmail.com>
2018-09-30 11:02:13 -0400

    Added redirect from app.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
               <Route path="/contact" component={Contact} />
               <Route exact path="/home" component={Home} />
             </Switch>
-            {/* <Redirect exact from="/" to="/home" component={Home} /> */}
+            <Redirect exact from="/" to="/home" component={Home} />
           </div>
         </div>
       </Router>
