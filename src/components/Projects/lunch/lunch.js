5de0b8843ced5e5e297744f2cdd7f6672e99fba0
Maximo DeLaRosa <mjd809@gmail.com>
2018-10-27 19:05:06 -0400

    Replaced Lunch with Park

--- src/components/Projects/lunch/lunch.js
+++ src/components/Projects/lunch/lunch.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import onClickOutside from 'react-onclickoutside';
 import Carousel from 'nuka-carousel';
 
-import lunch from './lunch.jpg';
+import park from './Park1.jpg';
 
 import '../Projects.css';
 
 class Lunch extends Component {
   state = { 
-    images: [ lunch ],
+    images: [ park ],
     translateValue: 0,
     currentIndex: 0,
    }
@@ -72,45 +72,41 @@ class Lunch extends Component {
   render() {
     const { currentIndex } = this.state;
     return (
-      <div className="Modal-box">
-           <div className="Slideshow"style={{
-            transform: `translateX(${this.state.translateValue}px)`,
-          }}>
-            {this.state.images.map((image, i) => 
-              <img key={i}src={image} style={{ visibility: i === currentIndex ? 'visible': 'hidden' }} className="Slideshow-lunch" alt="img" />  
-            )}
-          </div>
-          <div className="Slideshow-details">
-            <div className="Slideshow-arrows">
-              <div onClick={() => this.goToPrevSlide()}>
-                <i className="fas fa-arrow-left fa-fw adjust" />
-              </div>
-              <div onClick={() => this.goToNextSlide()}>
-                <i className="fas fa-arrow-right fa-fw opposite" />
-              </div>
+      <div className="Modal-box">      
+        <div className="Slideshow"style={{
+          transform: `translateX(${this.state.translateValue}px)`,
+        }}>
+          {this.state.images.map((image, i) => 
+            <img key={i}src={image} style={{ visibility: i === currentIndex ? 'visible': 'hidden' }} className="Slideshow-img" alt="img" />  
+          )}
+        </div>
+        <div className="Slideshow-details">
+          <div className="Slideshow-arrows">
+            <div onClick={() => this.goToPrevSlide()}>
+              <i className="fas fa-arrow-left fa-fw adjust" />
             </div>
-            <div className="SlideShow-title">
-              <p>Lunch</p>
-              <p className="Slideshow-details-sub">Food Application</p>
-            </div>
-            <hr className="hr"/>
-            <p className="Slideshow-description">
-              Lunch is a mobile application 
-              I built independently using React-Native, Python, Django-DRF, JWT to
-              manage the sites Authentication, and Sqlite.
-              <br />
-              <br />
-              Lunch, makes use of yelps API and maps the restaurants within
-              the given parameters. 
-            </p>
-            <div className="Slideshow-view">
-              <a href="https://github.com/Maxjoeld/Lunch" target="_blank" rel="noopener noreferrer">
-                <button  style={{ marginLeft: '1rem'}}className="Slideshow-viewcode">View Code</button>
-              </a>
-              <p className="leave-modal" onClick={() => this.props.showModal()}>x</p>
+            <div onClick={() => this.goToNextSlide()}>
+              <i className="fas fa-arrow-right fa-fw opposite" />
             </div>
           </div>
-        <input style={{ display: 'none'}}/>          
+          <div className="SlideShow-title">
+            <p>Park</p>
+            <p className="Slideshow-details-sub">Mobile application for free parking</p>
+          </div>
+          <hr className="hr"/>
+          <p className="Slideshow-description">
+            Mobile application that allows users to search for parking free of charge. The application relies on users to report when they are leaving their parking spot. Once the app receives a reqeust that a user is leaving, it alerts other users that are searching for parking spots within the specified radius.
+            <br />
+            The application makes use of the following API's: Google Matrix, Google Map, and Google Geocode
+          </p>
+          <div className="Slideshow-view">
+            <a href="https://github.com/Maxjoeld/Park" target="_blank" rel="noopener noreferrer">
+              <button style={{ marginLeft: '1rem'}} className="Slideshow-viewcode">View Code</button>
+            </a>
+            <p className="leave-modal" onClick={() => this.props.showModal()}>x</p>
+          </div>
+        </div>
+        <input style={{ display: 'none'}}/>    
       </div>     
     );
   }
