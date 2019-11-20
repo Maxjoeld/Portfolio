629d7b97f000f378d3d00bf224871dd2921ee56b
Maximo DeLaRosa <mjd809@gmail.com>
2019-11-20 11:57:13 -0500

    changed contact page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,32 +5,40 @@ import './App.css';
 class Home extends Component {
   state = {  }
   render() { 
-    return ( 
+    return (
       <div className="Home">
         <div className="Home-title">
-          Hello,<br /> 
+          Hello,
+          <br />
           I'm Maximo de la Rosa
           <div className="Home-subtext">
             <p>Software Developer</p>
             {/* <p>Graphic Designer</p>
             <p>/Hardware Consultant</p> */}
           </div>
-          <Link to='/contact'>
-            <button className="Contact-button">
-              Contact me
-            </button>
-          </Link>
+          <a
+            href="https://www.linkedin.com/in/maximo-delarosa/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="Contact-button">Contact me</button>
+          </a>
         </div>
         <div className="Home-box">
-          <p className="Homelogo">M<span style={{ marginLeft: '-170px'}}>D</span></p>
+          <p className="Homelogo">
+            M<span style={{ marginLeft: "-170px" }}>D</span>
+          </p>
         </div>
         <div className="nav-arrows">
-          <NavLink style={{ color: "white"}} to="/skills">
-            <i style={{ cursor: 'pointer' }}className="fas fa-arrow-right fa-fw pulse" />
+          <NavLink style={{ color: "white" }} to="/skills">
+            <i
+              style={{ cursor: "pointer" }}
+              className="fas fa-arrow-right fa-fw pulse"
+            />
           </NavLink>
         </div>
       </div>
-     );
+    );
   }
 }
  
